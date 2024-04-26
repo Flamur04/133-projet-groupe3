aec5efde86a254840cc04af5e8cd076f878d111e
Flamur04 <flamurhoti02@gmail.com>
2024-04-26 13:27:25 +0200

    Docker

---
+++ apigateway/src/main/java/com/example/apigateway/ApiController/rest1Controller.java
@@ -0,0 +1,5 @@
+package main.java.com.example.apigateway.ApiController;
+
+public class rest1Controller {
+    
+}
---
+++ apigateway/src/main/java/com/example/apigateway/ApiController/rest2Controller.java
@@ -0,0 +1,5 @@
+package main.java.com.example.apigateway.apiController;
+
+public class rest2Controller {
+    
+}
